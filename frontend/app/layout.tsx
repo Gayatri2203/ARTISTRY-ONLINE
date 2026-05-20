1350dd9bc481664b6845ac6fed102840afd1abc4
Gayatri <gayatripotdar0@gmail.com>
2026-05-20 23:25:19 +0530

    completion of login and signup

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -4,6 +4,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import ThemeRegistry from "@/src/theme/ThemeRegistry";
 
 import "./globals.css";
+import { AuthProvider } from "@/src/context/AuthContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -37,9 +38,11 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
       suppressHydrationWarning
     >
-      <body className="min-h-full flex flex-col">
-        <ThemeRegistry>{children}</ThemeRegistry>
-      </body>
+     <body className="min-h-full flex flex-col">
+  <AuthProvider>
+    <ThemeRegistry>{children}</ThemeRegistry>
+  </AuthProvider>
+</body>
     </html>
   );
 }
